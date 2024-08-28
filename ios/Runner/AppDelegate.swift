cc9b90bbff55c264d997ce389945036ec29da671
Daniil Alekseev <55713843+Dapucla@users.noreply.github.com>
2024-08-28 16:46:49 +0300

    TD-13: Info.plist and AppDelegate update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,15 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
-@UIApplicationMain
+
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyB09V4K-O3sQaG0ZwrNdKFZKsBSVEP-_vQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
