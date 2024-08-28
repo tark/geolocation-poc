c74111df4b20967e54692c214e50ee9f9daf6b62
Daniil Alekseev <55713843+Dapucla@users.noreply.github.com>
2024-08-28 15:30:04 +0300

    TD-13: android: app icon added

--- android/app/src/main/kotlin/com/example/geolocation_poc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/geolocation_poc/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.example.geolocation_poc
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import android.view.View
+import android.os.Build
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.navigationBarColor = android.graphics.Color.TRANSPARENT
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    )
+        }
+    }
+}
